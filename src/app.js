67c7eb764ed44bcc4a85b376ef38338452c08e0e
Justin Zhu <justinsihong@gmail.com>
2024-03-03 10:51:08 -0500

    collision func and gravity and floor collision
    added handle collision, only vertically so far, and gravity and floor
    collision added

--- src/app.js
+++ src/app.js
@@ -10,13 +10,29 @@ class GameScene extends Phaser.Scene {
         // adding floor
         this.floor = this.physics.add.staticGroup();
         this.floor.create(this.game.config.width/2, this.game.config.height, "floor");
-
         // adding players
         this.player1 = this.physics.add.sprite(150, 300, "player1");
         this.player2 = this.physics.add.sprite(850, 300, "player2");
+        this.player1.body.gravity.y = 500;
+        this.player2.body.gravity.y = 500;
+        this.physics.add.collider(this.player1, this.floor);
+        this.physics.add.collider(this.player2, this.floor);
+        this.physics.add.collider(this.player1, this.player2, () => {this.handleCollision();});
     }
     update() {
         
+    }
+    handleCollision(){
+        if (this.player1.y <= this.player2.y - this.player2.height/2)
+        {
+            this.player1.setVelocityY(-50);
+            this.player2.setVelocityY(100);
+        }
+        else if (this.player2.y <= this.player1.y - this.player1.height/2)
+        {
+            this.player2.setVelocityY(-50);
+            this.player1.setVelocityY(100);
+        }
     }
 }
 
