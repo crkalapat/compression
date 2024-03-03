c5b5411950a9fcf61db33cd13959e503489c863c
Daniel <getdanielmiller@gmail.com>
2024-03-03 15:03:43 -0500

    justin fucked up so Im fixing it

--- src/gameScene.js
+++ src/gameScene.js
@@ -4,6 +4,7 @@ class GameScene extends Phaser.Scene {
     constructor () {
         super("GameScene");
     }
+
     preload() {
         this.load.image("floor", "assets/floor.png");
         this.load.image("player1", "assets/Sprite-blue-front.png");
@@ -14,40 +15,36 @@ class GameScene extends Phaser.Scene {
         this.floor = this.physics.add.staticGroup();
         this.floor.create(this.game.config.width/2, this.game.config.height, "floor");
         // adding players
-        this.player1 = this.physics.add.sprite(150, 200, "player1");
-        this.player2 = this.physics.add.sprite(850, 200, "player2");
-        this.physics.add.collider(this.player1, this.floor);
-        this.physics.add.collider(this.player2, this.floor);
+        this.player1 = this.physics.add.sprite(150, 300, "player1");
+        this.player2 = this.physics.add.sprite(850, 300, "player2");
         this.player1.body.gravity.y = 500;
         this.player2.body.gravity.y = 500;
+        this.physics.add.collider(this.player1, this.floor);
+        this.physics.add.collider(this.player2, this.floor);
         this.physics.add.collider(this.player1, this.player2, () => {this.handleCollision();});
+
         this.player1.body.setSize(this.player1.width - 45, this.player1.height - 9, true);
         this.player2.body.setSize(this.player2.width - 45, this.player2.height - 9, true); 
+
+
     }
     update() {
-            this.movePlayer1();
-            this.movePlayer2();
+        this.movePlayer1();
+        this.movePlayer2();
     }
     handleCollision(){
-        if (this.player1.y == this.player2.y - this.player2.height)
+        if (this.player1.y <= this.player2.y - this.player2.height/2)
         {
-            this.player1.setVelocityY(-500);
-            this.player2.setVelocityY(500);
-
+            this.player1.setVelocityY(-300);
+            this.player2.setVelocityY(300);
         }
-        else if (this.player2.y == this.player1.y - this.player1.height)
+        else if (this.player2.y <= this.player1.y - this.player1.height/2)
         {
-            this.player2.setVelocityY(-500);
-            this.player1.setVelocityY(500);
-        }
-        else if (this.player1.x == this.player2.x - this.player2.width)
-        {
-            this.player1.setVelocityX(-500);
-            this.player2.setVelocityX(500);
+            this.player2.setVelocityY(-300);
+            this.player1.setVelocityY(300);
         }
     }
 
-    
     movePlayer1() {
         let keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         let keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -90,9 +87,6 @@ class GameScene extends Phaser.Scene {
         }  else {
             this.player2.body.velocity.x = 0;
         }
-    }
-    squish1() {
-        //this.player1 = this.physics.add.sprite(150, 200, "player1");
     }
 }
 
