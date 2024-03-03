c9656182d61d7abd2d5355472951dbac29dc3b41
Charlie Kalapati <charlie.r.kalapati@gmail.com>
2024-03-03 10:21:11 -0500

    Add P1 and P2

--- src/app.js
+++ src/app.js
@@ -3,17 +3,21 @@ import Phaser from "phaser";
 class GameScene extends Phaser.Scene {
     preload() {
         this.load.image("floor", "assets/floor.png");
+        this.load.image("player1", "assets/player1.png");
+        this.load.image("player2", "assets/player2.png");
     }
     create() {
         // adding floor
         this.floor = this.physics.add.staticGroup();
         this.floor.create(this.game.config.width/2, this.game.config.height, "floor");
-        this.physics.add.collider()
+
+        // adding players
+        this.player1 = this.physics.add.sprite(150, 300, "player1");
+        this.player2 = this.physics.add.sprite(850, 300, "player2");
     }
     update() {
         
     }
-
 }
 
 const config = {
