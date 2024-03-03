a3f22b50cd926c33c3f9b71b483bb3f52d00c431
Justin Zhu <justinsihong@gmail.com>
2024-03-03 14:50:54 -0500

    code moved to gameScene

--- src/gameScene.js
+++ src/gameScene.js
@@ -1,46 +1,50 @@
 import Phaser from "phaser";
 
 class GameScene extends Phaser.Scene {
-    constructor () {
-        super("GameScene");
-    }
-
     preload() {
         this.load.image("floor", "assets/floor.png");
-        this.load.image("player1", "assets/player1.png");
-        this.load.image("player2", "assets/player2.png");
+        this.load.image("player1", "assets/Sprite-blue-front.png");
+        this.load.image("player2", "assets/Sprite-red-front.png");
     }
     create() {
         // adding floor
         this.floor = this.physics.add.staticGroup();
         this.floor.create(this.game.config.width/2, this.game.config.height, "floor");
         // adding players
-        this.player1 = this.physics.add.sprite(150, 300, "player1");
-        this.player2 = this.physics.add.sprite(850, 300, "player2");
-        this.player1.body.gravity.y = 500;
-        this.player2.body.gravity.y = 500;
+        this.player1 = this.physics.add.sprite(150, 200, "player1");
+        this.player2 = this.physics.add.sprite(850, 200, "player2");
         this.physics.add.collider(this.player1, this.floor);
         this.physics.add.collider(this.player2, this.floor);
+        this.player1.body.gravity.y = 500;
+        this.player2.body.gravity.y = 500;
         this.physics.add.collider(this.player1, this.player2, () => {this.handleCollision();});
-
+        this.player1.body.setSize(this.player1.width - 50, this.player1.height - 8, true);
+        this.player2.body.setSize(this.player2.width - 50, this.player2.height - 8, true);
     }
     update() {
-        this.movePlayer1();
-        this.movePlayer2();
+            this.movePlayer1();
+            this.movePlayer2();
     }
     handleCollision(){
-        if (this.player1.y <= this.player2.y - this.player2.height/2)
+        if (this.player1.y <= this.player2.y - this.player2.height)
         {
-            this.player1.setVelocityY(-300);
-            this.player2.setVelocityY(300);
+            this.player1.setVelocityY(-500);
+            this.player2.setVelocityY(500);
+
         }
-        else if (this.player2.y <= this.player1.y - this.player1.height/2)
+        else if (this.player2.y <= this.player1.y - this.player1.height)
         {
-            this.player2.setVelocityY(-300);
-            this.player1.setVelocityY(300);
+            this.player2.setVelocityY(-500);
+            this.player1.setVelocityY(500);
+        }
+        else if (this.player1.x == this.player2.x - this.player2.width)
+        {
+            this.player1.setVelocityX(-1000);
+            this.player2.setVelocityX(1000);
         }
     }
 
+    
     movePlayer1() {
         let keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         let keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -83,6 +87,9 @@ class GameScene extends Phaser.Scene {
         }  else {
             this.player2.body.velocity.x = 0;
         }
+    }
+    squish1() {
+        //this.player1 = this.physics.add.sprite(150, 200, "player1");
     }
 }
 
