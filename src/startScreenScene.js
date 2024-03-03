f2832fb94f685a4355a5997d6b60e86cf55c41bd
Daniel <getdanielmiller@gmail.com>
2024-03-03 15:10:51 -0500

    better start screen

--- src/startScreenScene.js
+++ src/startScreenScene.js
@@ -11,20 +11,43 @@ class StartScreenScene extends Phaser.Scene {
     create() {
         let bg = this.add.sprite(0, 0, "bg");
         bg.setOrigin(0, 0); 
-
-        let startLabel = this.add.text(
+        let nameLabel = this.add.text(
+            this.game.config.width / 2,
+            this.game.config.height / 2,
+            "Compression",
+            { font: "100px Arial", fill: "#000000"}
+        );
+        nameLabel.setOrigin(0.5, 0.5);
+        
+        let instructionsLabel = this.add.text(
             this.game.config.width / 2,
             this.game.config.height / 3,
+            "Blue: WASD           Red: ↑ ↓ → ←",
+            { font: "30px Arial", fill: "#000000"}
+        );
+        instructionsLabel.setOrigin(0.5, 0.5);
+
+        let instructionsLabel2 = this.add.text(
+            this.game.config.width / 2,
+            this.game.config.height / 5,
+            "Jump on your friends to crush their hopes and dreams (and them)",
+            { font: "30px Arial", fill: "#000000"}
+        );
+        instructionsLabel2.setOrigin(0.5, 0.5);
+
+        let startLabel = this.add.text(
+            (2 * this.game.config.width) / 6,
+            (2 * this.game.config.height) / 3 + 50,
             "Press Enter To Start",
-            { font: "40px Arial", fill: "#000000"}
+            { font: "30px Arial", fill: "#000000"}
         );
         startLabel.setOrigin(0.5, 0.5);
 
         let exitLabel = this.add.text(
-            this.game.config.width / 2,
-            (2 * this.game.config.height) / 3,
+            (4 * this.game.config.width) / 6,
+            (2 * this.game.config.height) / 3 + 50,
             "Press Backspace To Exit",
-            { font: "40px Arial", fill: "#000000"}
+            { font: "30px Arial", fill: "#000000"}
         );
         exitLabel.setOrigin(0.5, 0.5);
 
