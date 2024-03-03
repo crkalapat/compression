7d4f8c6bf8aff5a9b9ba44b6741d383cf551c702
Charlie Kalapati <charlie.r.kalapati@gmail.com>
2024-03-03 11:53:39 -0500

    Add controls

--- src/app.js
+++ src/app.js
@@ -18,20 +18,66 @@ class GameScene extends Phaser.Scene {
         this.physics.add.collider(this.player1, this.floor);
         this.physics.add.collider(this.player2, this.floor);
         this.physics.add.collider(this.player1, this.player2, () => {this.handleCollision();});
+
     }
     update() {
-        
+        this.movePlayer1();
+        this.movePlayer2();
     }
     handleCollision(){
         if (this.player1.y <= this.player2.y - this.player2.height/2)
         {
-            this.player1.setVelocityY(-50);
-            this.player2.setVelocityY(100);
+            this.player1.setVelocityY(-300);
+            this.player2.setVelocityY(300);
         }
         else if (this.player2.y <= this.player1.y - this.player1.height/2)
         {
-            this.player2.setVelocityY(-50);
-            this.player1.setVelocityY(100);
+            this.player2.setVelocityY(-300);
+            this.player1.setVelocityY(300);
+        }
+    }
+
+    movePlayer1() {
+        let keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        let keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        let keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        let keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
+        if (keyW.isDown && this.player1.body.onFloor()) {
+            this.player1.body.velocity.y = -400;
+        }
+
+        if (keyS.isDown) {
+            this.player1.body.velocity.y = 400;
+        }
+
+        if (keyA.isDown) {
+            this.player1.body.velocity.x = -200;
+        } else if (keyD.isDown) {
+            this.player1.body.velocity.x = 200;
+        }  else {
+            this.player1.body.velocity.x = 0;
+        }
+    }
+
+    movePlayer2() {
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        if (this.cursors.up.isDown && this.player2.body.onFloor()) {
+            this.player2.body.velocity.y = -400;
+        }
+
+        if (this.cursors.down.isDown) {
+            this.player2.body.velocity.y = 400;
+        }
+
+        if (this.cursors.left.isDown) {
+            this.player2.body.velocity.x = -200;
+        } else if (this.cursors.right.isDown) {
+            this.player2.body.velocity.x = 200;
+        }  else {
+            this.player2.body.velocity.x = 0;
         }
     }
 }
